fb35ae8846bb38b4b3b16ccf4232d9b567ef00f6
Irfan Yousuf <irfanusuf33@gmail.com>
2025-11-18 16:00:09 +0530

    register

--- 2. Express_server/index.js
+++ 2. Express_server/index.js
@@ -2,6 +2,7 @@ const express = require("express")   // importing express library
 const connectDb = require("./config/connectDB")  // import 
 const { registerHandler } = require("./controllers/userController")
 const bodyParser = require("body-parser")
+const cors = require("cors")
 
 
 
@@ -13,18 +14,16 @@ connectDb()
 
 // middle ware 
 app.use(bodyParser.json())
+app.use(cors())   // ready made cors policy used in middleware
 
 
 
 
 app.get("/" , (req,res)=>{res.send("hello from the server ")  })
 
-
 app.get("/user/register" , (req,res)=>{ res.send("register not Working !")})
 
 app.post("/user/register" , registerHandler )
-
-
 
 
 app.listen(port  , ()=>{console.log("server listening on port 4000")} )
